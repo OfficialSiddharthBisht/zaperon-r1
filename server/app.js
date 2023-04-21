eba80553062728791a7044996f8e30fe5ccf3bc7
Siddharth <officialsiddharthbisht@gmail.com>
2023-04-21 20:52:53 +0530

    feat: prevented errors

--- server/app.js
+++ server/app.js
@@ -2,7 +2,12 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 
+const errorMiddleware = require("./middlewares/error");
+
 app.use(express.json());
 app.use(cookieParser());
+
+// Middleware for Errors
+app.use(errorMiddleware);
 
 module.exports = app;
